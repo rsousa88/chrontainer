8c64d7e67162610e4b70eb6bfacfad3ce590f909
Rui Sousa <88rsousa@gmail.com>
2026-01-29 00:13:27 +0000

    Add host metrics repository implementation

--- app/repositories/hosts_metrics.py
+++ app/repositories/hosts_metrics.py
@@ -18,3 +18,17 @@ class HostMetricsRepository:
             return cursor.fetchall()
         finally:
             conn.close()
+
+    def get_metrics(self, docker_client):
+        info = docker_client.info()
+        total_mem = info.get('MemTotal', 0) or 0
+        total_mem_gb = round(total_mem / (1024**3), 2) if total_mem else 0
+        return {
+            'os': info.get('OperatingSystem', 'Unknown'),
+            'docker_version': info.get('ServerVersion', 'Unknown'),
+            'cpus': info.get('NCPU', 0),
+            'total_memory_gb': total_mem_gb,
+            'containers_running': info.get('ContainersRunning', 0),
+            'containers_stopped': info.get('ContainersStopped', 0),
+            'containers_paused': info.get('ContainersPaused', 0),
+        }
